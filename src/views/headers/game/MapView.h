2b48a16f3299cbbb7102a19740a702c037984e52
Sailor <la237887@student.helha.be>
2025-12-29 19:20:19 +0100

    map 1,2,3

---
+++ src/views/headers/game/MapView.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "../../../models/headers/TileMap.h"
+
+class MapView : public sf::Drawable, public sf::Transformable {
+public:
+    bool load(const TileMap& map);
+
+private:
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+private:
+    sf::VertexArray m_vertices;
+    sf::Texture m_tileset; // <-- C'est ici qu'on va stocker l'image
+};
